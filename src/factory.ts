e33c2f8eb01b3f6b29df5aad3249814a31ce565c
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-03 00:03:58 -0300

    refactor: simplify DataRepository instantiation in Factory class

--- src/factory.ts
+++ src/factory.ts
@@ -1,11 +1,9 @@
 import { StudentController } from "@controllers/student.controller";
 import { StudentService } from "@services/student.service";
-import DataRepository, {dataSource} from "data";
+import DataRepository from "data";
 
-export default class Factory{
-
-    public static createStudentController(){
-        return new StudentController(new StudentService(new DataRepository(dataSource)));
-    }
-        
+export default class Factory {
+  public static createStudentController() {
+    return new StudentController(new StudentService(DataRepository.getInstance()));
+  }
 }

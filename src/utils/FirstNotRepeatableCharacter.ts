e75b207450ccbc57471bb23709e0463d9bf1a32e
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-02 22:09:49 -0300

    feat: add function to find the first non-repeating character in a string

---
+++ src/utils/FirstNotRepeatableCharacter.ts
@@ -0,0 +1,15 @@
+export function FindFirstNotRepeatableCharacter(str: string){
+    str = str.toLowerCase();
+    const charCount: Record<string, number> = {};
+
+    for (const char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    };
+
+    for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    };
+    return "_";
+}
